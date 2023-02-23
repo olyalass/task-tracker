5bc1d4b101e9323380aad59c75476430cad912d8
olyalass <olyalass@icloud.com>
2023-02-23 18:35:02 +0100

    refactor: fix typos in var&class names

--- src/components/nav/index.ts
+++ src/components/nav/index.ts
@@ -6,20 +6,24 @@ export class Nav {
   buttonAll: HTMLButtonElement;
   buttonToDo: HTMLButtonElement;
   buttonDone: HTMLButtonElement;
-  buttonRevert: HTMLButtonElement;
+  buttonReverse: HTMLButtonElement;
+  isReversed: boolean = false;
 
   constructor () {
     this.container = document.createElement('div');
     this.container.classList.add('nav');
     this.buttonsContainer = document.createElement('div');
     this.buttonsContainer.classList.add('nav__container');
-    this.buttonRevert = document.createElement('button');
-    this.buttonRevert.classList.add('nav__button');
-    this.container.append(this.buttonsContainer, this.buttonRevert);
+    this.buttonReverse = document.createElement('button');
+    this.buttonReverse.classList.add('nav__button');
+    this.container.append(this.buttonsContainer, this.buttonReverse);
 
     const buttonImg = document.createElement('div');
-    buttonImg.classList.add('nav__revert');
-    this.buttonRevert.appendChild(buttonImg);
+    buttonImg.classList.add('nav__reverse');
+    this.buttonReverse.appendChild(buttonImg);
+    this.buttonReverse.addEventListener("click", () => {
+      this.isReversed === false ? this.isReversed = true : this.isReversed = false;
+    })
     
 
     this.buttonAll = document.createElement('button');
@@ -27,7 +31,7 @@ export class Nav {
     this.buttonAll.textContent = "All";
     this.buttonToDo = document.createElement('button');
     this.buttonToDo.classList.add('nav__button');
-    this.buttonToDo.textContent = "To-do";
+    this.buttonToDo.textContent = "Active";
     this.buttonDone = document.createElement('button');
     this.buttonDone.classList.add('nav__button');
     this.buttonDone.textContent = "Done";
